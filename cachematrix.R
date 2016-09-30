8399f948e529b984605f1c0069512580170c9908
Estevan Gomez <egtgomez@yahoo.com>
2016-09-30 07:55:57 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## (1)get is a function that returns the vector x stored in the main function.
+##(2)set is a function that changes the vector stored in the main function.
+##(3)setmean and getmean are functions very similar to set and get.
+##(4)They don’t calculate the mean, they simply store the value of the input in a variable m.
+##(5)into the main function makeVector (setmean) and return it (getmean).
 
-## Write a short comment describing this function
+## Author: Estevan Gomez
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
+      list(set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
 
 
